29a43426684d6e1413680ad3b80e6e28545f94a7
Alexandr Birichevskiy <alexandrbirichevskiy@gmail.com>
2021-06-15 01:23:13 +0300

    Add CardView

--- app/src/main/java/com/alexbirichevskiy/notes/MyAdapter.java
+++ app/src/main/java/com/alexbirichevskiy/notes/MyAdapter.java
@@ -4,13 +4,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     private Notes[] notes;
+    private OnItemClickListener listener;
 
     MyAdapter(Notes[] notes) {
         this.notes = notes;
@@ -28,7 +28,7 @@ class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position) {
-        holder.bind(notes[position].getName());
+        holder.bind(notes[position].getName(), notes[position].getDate());
     }
 
     @Override
@@ -36,17 +36,36 @@ class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         return notes.length;
     }
 
-    static class MyViewHolder extends RecyclerView.ViewHolder{
+    public void setListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
 
-        private TextView textView;
+    class MyViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView textViewName;
+        private TextView textViewDate;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            textView = itemView.findViewById(R.id.text_view_items);
+            textViewName = itemView.findViewById(R.id.text_view_name);
+            textViewDate = itemView.findViewById(R.id.text_view_date);
         }
 
-        void bind(String text) {
-            textView.setText(text);
+        void bind(String textName, String textDate) {
+            textViewName.setText(textName);
+            textViewDate.setText(textDate);
+            textViewName.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null){
+                        listener.onItemClick(getAdapterPosition());
+                    }
+                }
+            });
         }
+    }
+
+    interface OnItemClickListener {
+        void onItemClick(int position);
     }
 }
